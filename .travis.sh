046624cdda20c8aeeaa8ceb4b3bbf2f4fe27a398
Jakub Czekański <ja.czekanski@gmail.com>
2015-07-14 21:13:35 +0200

    Should work now

---
+++ .travis.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf build/
+mkdir -p build
+$CXX src/main.cpp src/mipsInstructions.cpp src/utils/file.cpp src/utils/string.cpp `sdl2-config --cflags --libs` -std=c++11 -Wno-write-strings -o build/avocado
