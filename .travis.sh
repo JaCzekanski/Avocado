722044aa5ab8ef59d4d69f40d965bf0503c3e96a
Jakub Czekański <ja.czekanski@gmail.com>
2015-07-18 12:54:45 +0200

    duh...

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SRC=`find src/ | grep '\(\.cpp$\)'`
-CXXFLAGS=-std=c++11 -Wno-write-strings `sdl2-config --cflags --libs`
-OUTPUT=build/avocado
+CXXFLAGS="-std=c++11 -Wno-write-strings `sdl2-config --cflags --libs`"
+OUTPUT="build/avocado"
 rm -rf build/
 mkdir -p build
 $CXX --version
