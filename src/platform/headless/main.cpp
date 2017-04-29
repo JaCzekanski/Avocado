84e0c0f0348f287a1d8b4f4b35cf953774f1e6fb
Jakub Czekański <ja.czekanski@gmail.com>
2017-04-29 23:46:28 +0200

    forgot to update headless main

--- src/platform/headless/main.cpp
+++ src/platform/headless/main.cpp
@@ -6,24 +6,22 @@
 #include "mips.h"
 
 int main(int argc, char **argv) {
-	if (argc < 2) {
-		printf("usage: Avocado psx.exe\n");
-		return 1;
-	}
+    if (argc < 2) {
+        printf("usage: Avocado psx.exe\n");
+        return 1;
+    }
     std::unique_ptr<mips::CPU> cpu = std::make_unique<mips::CPU>();
-	auto gpu = std::make_unique<device::gpu::GPU>();
-    cpu->setGPU(gpu.get());
 
-	if (!cpu->loadBios("SCPH1001.BIN")) {
+    if (!cpu->loadBios("SCPH1001.BIN")) {
         return 1;
     }
 
-	if (!cpu->loadExpansion("data/asm/bootstrap.bin")) {
-		printf("cannot load expansion!\n");
-		return 1;
-	}
+    if (!cpu->loadExpansion("data/asm/bootstrap.bin")) {
+        printf("cannot load expansion!\n");
+        return 1;
+    }
 
-	cpu->biosLog = false;
+    cpu->biosLog = false;
     cpu->debugOutput = false;
 
     // Emulate BIOS to GUI breakpoint
@@ -32,13 +30,13 @@ int main(int argc, char **argv) {
     }
 
     if (!cpu->loadExeFile(argv[1])) {
-		printf("Cannot load %s\n", argv[1]);
-		return 1;
-   	}
+        printf("Cannot load %s\n", argv[1]);
+        return 1;
+    }
     printf("File %s loaded\n", getFilenameExt(argv[1]).c_str());
 
-	cpu->state = mips::CPU::State::run;
-	cpu->debugOutput = false;
+    cpu->state = mips::CPU::State::run;
+    cpu->debugOutput = false;
     cpu->PC = cpu->readMemory32(0x1f000000);
 
     while (cpu->state == mips::CPU::State::run) {
