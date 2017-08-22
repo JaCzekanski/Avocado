ede44e74c2a6e1018da0037296bbc5ffd0d88f81
Jakub Czekański <ja.czekanski@gmail.com>
2017-08-22 19:09:32 +0200

    extracted gui stuff to separate files

---
+++ src/platform/windows/gui.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "mips.h"
+
+void renderImgui(mips::CPU *cpu);
+extern bool gteRegistersEnabled;
+extern bool ioLogEnabled;
+extern bool gteLogEnabled;
+extern bool gpuLogEnabled;
+extern bool showVRAM;
+extern bool singleFrame;
+extern bool skipRender;
+extern bool showIo;
+extern bool exitProgram;
