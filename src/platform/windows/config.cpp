b3539226ba3b16d5331d76a6470f7c48dce59fce
Jakub Czekański <ja.czekanski@gmail.com>
2017-09-02 18:48:40 +0200

    added json type checker, will add missing fields and repair types

--- src/platform/windows/config.cpp
+++ src/platform/windows/config.cpp
@@ -3,7 +3,14 @@
 
 const char* CONFIG_NAME = "config.json";
 
-nlohmann::json config = {{"initialized", false}, {"bios", ""}, {"extension", ""}, {"iso", ""}};
+// clang-format off
+nlohmann::json config = {
+	{"initialized", false}, 
+	{"bios", ""}, 
+	{"extension", ""}, 
+	{"iso", ""}
+};
+// clang-format on
 
 void saveConfigFile(const char* configName) { putFileContents(configName, config.dump(4)); }
 
@@ -13,5 +20,25 @@ void loadConfigFile(const char* configName) {
         saveConfigFile(configName);
         return;
     }
-    config = nlohmann::json::parse(file);
+    nlohmann::json newconfig = nlohmann::json::parse(file);
+
+    for (auto it = config.begin(); it != config.end(); ++it) {
+        auto field = newconfig.find(it.key());
+
+        // Add nonexisting fields
+        if (field == newconfig.end()) {
+            printf("Config: No field %s \n", it.key().c_str());
+            newconfig.emplace(it.key(), it.value());
+            continue;
+        }
+
+        // Repair these with invalid types
+        if (field.value().type() != it.value().type()) {
+            printf("Config: Invalid type of %s (%s), changing to %s\n", field.key().c_str(), field.value().type_name().c_str(),
+                   it.value().type_name().c_str());
+            newconfig[it.key()] = it.value();
+        }
+    }
+
+    config = newconfig;
 }
