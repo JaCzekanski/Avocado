3e3a524897dad18284cb074bdb3f6bd947ce232d
Jakub Czekański <ja.czekanski@gmail.com>
2018-02-26 00:44:27 +0100

    render: PSXColor improvements

--- src/device/gpu/psx_color.h
+++ src/device/gpu/psx_color.h
@@ -48,38 +48,38 @@ union PSXColor {
     PSXColor() : _(0) {}
     PSXColor(uint16_t color) : _(color) {}
 
-    PSXColor operator+(PSXColor rhs) {
-        r = clamp<int>(r + rhs.r, 0, 31);
-        g = clamp<int>(g + rhs.g, 0, 31);
-        b = clamp<int>(b + rhs.b, 0, 31);
+    PSXColor operator+(const PSXColor& rhs) {
+        r = std::min(r + rhs.r, 31);
+        g = std::min(g + rhs.g, 31);
+        b = std::min(b + rhs.b, 31);
         return *this;
     }
 
-    PSXColor operator-(PSXColor rhs) {
-        r = clamp<int>(r - rhs.r, 0, 31);
-        g = clamp<int>(g - rhs.g, 0, 31);
-        b = clamp<int>(b - rhs.b, 0, 31);
+    PSXColor operator-(const PSXColor& rhs) {
+        r = std::max<int>(r - rhs.r, 0);
+        g = std::max<int>(g - rhs.g, 0);
+        b = std::max<int>(b - rhs.b, 0);
         return *this;
     }
 
-    PSXColor operator*(float rhs) {
+    PSXColor operator*(const float& rhs) {
         r = (uint16_t)(r * rhs);
         g = (uint16_t)(g * rhs);
         b = (uint16_t)(b * rhs);
         return *this;
     }
 
-    PSXColor operator/(float rhs) {
+    PSXColor operator/(const float& rhs) {
         r = (uint16_t)(r / rhs);
         g = (uint16_t)(g / rhs);
         b = (uint16_t)(b / rhs);
         return *this;
     }
 
-    PSXColor operator*(glm::vec3 rhs) {
-        r = clamp<uint16_t>((uint16_t)(rhs.r / 0.5f * r), 0, 31);
-        g = clamp<uint16_t>((uint16_t)(rhs.g / 0.5f * g), 0, 31);
-        b = clamp<uint16_t>((uint16_t)(rhs.b / 0.5f * b), 0, 31);
+    PSXColor operator*(const glm::vec3& rhs) {
+        r = std::min<uint16_t>((uint16_t)(rhs.r / 0.5f * r), 31);
+        g = std::min<uint16_t>((uint16_t)(rhs.g / 0.5f * g), 31);
+        b = std::min<uint16_t>((uint16_t)(rhs.b / 0.5f * b), 31);
         return *this;
     }
 };
--- src/device/gpu/texture_utils.h
+++ src/device/gpu/texture_utils.h
@@ -3,13 +3,13 @@
 
 #define gpuVRAM ((uint16_t(*)[VRAM_WIDTH])gpu->vram.data())
 
-uint16_t tex4bit(GPU* gpu, glm::ivec2 tex, glm::ivec2 texPage, glm::ivec2 clut) {
+inline uint16_t tex4bit(GPU* gpu, glm::ivec2 tex, glm::ivec2 texPage, glm::ivec2 clut) {
     uint16_t index = gpuVRAM[texPage.y + tex.y][texPage.x + tex.x / 4];
     uint16_t entry = (index >> ((tex.x & 3) * 4)) & 0xf;
     return gpuVRAM[clut.y][clut.x + entry];
 }
 
-uint16_t tex8bit(GPU* gpu, glm::ivec2 tex, glm::ivec2 texPage, glm::ivec2 clut) {
+inline uint16_t tex8bit(GPU* gpu, glm::ivec2 tex, glm::ivec2 texPage, glm::ivec2 clut) {
     uint16_t index = gpuVRAM[texPage.y + tex.y][texPage.x + tex.x / 2];
     uint16_t entry = (index >> ((tex.x & 1) * 8)) & 0xff;
     return gpuVRAM[clut.y][clut.x + entry];
