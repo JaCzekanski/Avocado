00422c8408e21d7670550e8e1bcb9cd7b1e58242
Jakub Czekański <ja.czekanski@gmail.com>
2018-09-14 20:37:39 +0200

    serial: read/write baud register (fix Wipeout boot)

--- src/device/serial.cpp
+++ src/device/serial.cpp
@@ -9,13 +9,19 @@ void Serial::reset() { status._reg = 0x00000005; }
 
 uint8_t Serial::read(uint32_t address) {
     if (address >= 4 && address < 8) {
-        return status._reg;
+        return status._byte[address - 4];
+    }
+    if (address >= 14 && address < 16) {
+        return baud._byte[address - 14];
     }
 
-    // printf("Serial read @ 0x%02x\n", address);
+    printf("[SERIAL] read @ 0x%02x\n", address);
     return 0;
 }
 
 void Serial::write(uint32_t address, uint8_t data) {
-    // printf("Serial write @ 0x%02x: 0x%02x\n", address, data);
+    printf("[SERIAL] write @ 0x%02x: 0x%02x\n", address, data);
+    if (address >= 14 && address < 16) {
+        baud._byte[address - 14] = data;
+    }
 }
