c9b7b8b474452ed50eaa5befda75913cf35afaf1
Jakub Czekański <ja.czekanski@gmail.com>
2020-02-04 00:53:14 +0100

    savestate: bump to version 4 after SPU refactor

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -21,7 +21,7 @@ const std::string basePath = "data/state/";
 const std::string lastSavePath = basePath + "last.state";
 
 struct StateMetadata {
-    inline static const uint32_t SAVESTATE_VERSION = 3;
+    inline static const uint32_t SAVESTATE_VERSION = 4;
 
     uint32_t version = SAVESTATE_VERSION;
     std::string biosPath;
