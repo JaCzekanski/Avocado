a28686b77c8f1b31204adc239b7869c9591f7c6c
mpc <mpc@notamail.com>
2020-11-19 11:25:15 +0300

    Extra qualification on member 'getName' fix

--- src/platform/windows/input/key.h
+++ src/platform/windows/input/key.h
@@ -37,7 +37,7 @@ struct Key {
     static Key mouseButton(SDL_MouseButtonEvent button);
     static Key controllerMove(SDL_ControllerAxisEvent axis);
     static Key controllerButton(SDL_ControllerButtonEvent button);
-    const char* Key::getName();
+    const char* getName();
 
     bool operator==(const Key& rhs) const;
 
