7d83097357220e640ef2a8459b660b3f2a594bd3
Jakub Czekański <ja.czekanski@gmail.com>
2020-12-23 11:22:14 +0100

    fifo: fixed length off-by-one error in implementation

--- src/device/cdrom/fifo.h
+++ src/device/cdrom/fifo.h
@@ -7,6 +7,7 @@ class fifo {
     T data[length] = {};
     size_t write_ptr = 0;
     size_t read_ptr = 0;
+    bool full = false;
 
    public:
     size_t size() const {
@@ -17,34 +18,38 @@ class fifo {
         }
     }
 
-    bool empty() const { return size() == 0; }
+    bool is_empty() const { return write_ptr == read_ptr && !full; }
 
-    bool full() const { return size() == length - 1; }
+    bool is_full() const { return full; }
 
     void clear() {
         write_ptr = 0;
         read_ptr = 0;
+        full = false;
     }
 
     bool add(const T t) {
-        if (full()) {
+        if (is_full()) {
             return false;
         }
 
         data[write_ptr] = t;
-        write_ptr = ++write_ptr % length;
+        write_ptr = (write_ptr + 1) % length;
+
+        full = write_ptr == read_ptr;
 
         return true;
     }
 
     T get() {
-        if (empty()) {
+        if (is_empty()) {
             return 0;
-            // TODO ?
         }
 
         T t = data[read_ptr];
-        read_ptr = ++read_ptr % length;
+        read_ptr = (read_ptr + 1) % length;
+
+        full = false;
 
         return t;
     }
@@ -52,7 +57,6 @@ class fifo {
     T peek(const size_t ptr = 0) const {
         if (ptr >= size()) {
             return 0;
-            // TODO ?
         }
 
         return data[(read_ptr + ptr) % length];
@@ -62,6 +66,6 @@ class fifo {
 
     template <class Archive>
     void serialize(Archive& ar) {
-        ar(data);
+        ar(data, write_ptr, read_ptr, full);
     }
 };
