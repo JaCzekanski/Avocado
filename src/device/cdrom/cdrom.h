a27b6fd53a96fdc916724d2dab4d73cb7001f9bf
Jakub Czekański <ja.czekanski@gmail.com>
2021-01-02 16:59:22 +0100

    cdrom: fixed interrupt/response queue

--- src/device/cdrom/cdrom.h
+++ src/device/cdrom/cdrom.h
@@ -62,11 +62,11 @@ class CDROM {
     union CDROM_Status {
         struct {
             uint8_t index : 2;
-            uint8_t xaFifoEmpty : 1;
-            uint8_t parameterFifoEmpty : 1;  // triggered before writing first byte
-            uint8_t parameterFifoFull : 1;   // triggered after writing 16 bytes
-            uint8_t responseFifoEmpty : 1;   // triggered after reading last byte
-            uint8_t dataFifoEmpty : 1;       // triggered after reading last byte
+            uint8_t xaFifoEmpty : 1;         // 1 - NOT empty, 0 - empty, INVERSE LOGIC
+            uint8_t parameterFifoEmpty : 1;  // 1 - empty, 0 - not empty
+            uint8_t parameterFifoFull : 1;   // 1 - NOT full, 0 - full, INVERSE LOGIC
+            uint8_t responseFifoEmpty : 1;   // 1 - NOT empty, 0 - empty, INVERSE LOGIC
+            uint8_t dataFifoEmpty : 1;       // 1 - NOT empty, 0 - empty, INVERSE LOGIC
             uint8_t transmissionBusy : 1;
         };
         uint8_t _reg;
@@ -102,6 +102,18 @@ class CDROM {
         }
     };
 
+    // Hack
+    struct irq_response_t {
+        uint8_t irq;
+        fifo<uint8_t, 16> response;
+        bool ack;
+
+        template <class Archive>
+        void serialize(Archive& ar) {
+            ar(irq, response, ack);
+        }
+    };
+
     using FIFO = fifo<uint8_t, 16>;
 
     int verbose = 1;
@@ -109,8 +121,7 @@ class CDROM {
     CDROM_Status status;
     uint8_t interruptEnable = 0;
     FIFO CDROM_params;
-    FIFO CDROM_response;
-    FIFO interruptQueue;
+    fifo<irq_response_t, 16> interruptQueue;
 
     Mode mode;
     Filter filter;
@@ -154,28 +165,14 @@ class CDROM {
     void cmdSeekP();
     void handleCommand(uint8_t cmd);
 
-    void writeResponse(uint8_t byte) {
-        if (CDROM_response.is_full()) {
-            return;
-        }
-        CDROM_response.add(byte);
-        status.responseFifoEmpty = 1;
-    }
+    void writeResponse(uint8_t byte);
 
     uint8_t readParam() {
         uint8_t param = CDROM_params.get();
-
-        status.parameterFifoEmpty = CDROM_params.is_empty();
-        status.parameterFifoFull = 1;
-
         return param;
     }
 
-    void postInterrupt(int irq) {
-        assert(irq <= 7);
-
-        interruptQueue.add(irq);
-    }
+    void postInterrupt(int irq) { interruptQueue.add(irq_response_t{.irq = (uint8_t)irq, .response = {}}); }
 
     std::string dumpFifo(const FIFO& f);
     std::pair<int16_t, int16_t> mixSample(std::pair<int16_t, int16_t> sample);
@@ -213,7 +210,7 @@ class CDROM {
     void serialize(Archive& ar) {
         ar(status._reg, interruptEnable);
         ar(CDROM_params);
-        ar(CDROM_response);
+        //        ar(CDROM_response);
         ar(interruptQueue);
         ar(mode._reg);
         ar(filter);
