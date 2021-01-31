e33cbf5aef8d9a7558ec5fc50fbddd9c6fadb207
Jakub Czekański <ja.czekanski@gmail.com>
2021-01-31 01:19:52 +0100

    cdrom: 75/150 sectors per second speed

--- src/utils/timing.h
+++ src/utils/timing.h
@@ -13,14 +13,14 @@ const float CYCLES_PER_LINE_PAL = 3406.1f;
 const int LINES_TOTAL_NTSC = 263;
 const int LINES_TOTAL_PAL = 314;
 #else
-const float CYCLES_PER_LINE_NTSC = 3372;
+const float CYCLES_PER_LINE_NTSC = 3372.7f;
 const float CYCLES_PER_LINE_PAL = 3389;
 const int LINES_TOTAL_NTSC = 263;
 const int LINES_TOTAL_PAL = 314;
 #endif
 
-const float NTSC_FRAMERATE = (float)GPU_CLOCK / (CYCLES_PER_LINE_NTSC * LINES_TOTAL_NTSC);
-const float PAL_FRAMERATE = (float)GPU_CLOCK / (CYCLES_PER_LINE_PAL * LINES_TOTAL_PAL);
+const double NTSC_FRAMERATE = (double)GPU_CLOCK / (CYCLES_PER_LINE_NTSC * LINES_TOTAL_NTSC);
+const double PAL_FRAMERATE = (double)GPU_CLOCK / (CYCLES_PER_LINE_PAL * LINES_TOTAL_PAL);
 
 constexpr uint64_t usToCpuCycles(uint64_t us) { return us * CPU_CLOCK / US_IN_SECOND; }
 }  // namespace timing
